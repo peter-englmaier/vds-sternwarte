0fd49d2ecbfd4d571b98ba358c4381680929bbe8
Peter Englmaier <peter@englmaier.ch>
2024-09-08 21:18:50 +0200

    Replace gunicorn with waitress-serv

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 flask db upgrade
-exec gunicorn prod:app -b 0.0.0.0:5000 -w 1 --access-logfile -
+#exec gunicorn prod:app -b 0.0.0.0:5000 -w 1 --access-logfile -
+exec waitress-serv --listen 0.0.0.0:5000 app:app
 
