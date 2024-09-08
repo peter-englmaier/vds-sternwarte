5b6745b2f707baa01032c1c2e1be04b28d667bea
Peter Englmaier <peter@englmaier.ch>
2024-09-08 21:19:17 +0200

    In production we are behind a proxy

--- prod.py
+++ prod.py
@@ -1,4 +1,10 @@
 from webapp import create_app
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
+
 app = create_app()
 
