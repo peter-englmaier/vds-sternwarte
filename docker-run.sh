c3b1418b71bbe8da00686c870d01457156dfd5f5
Peter Englmaier <peter@englmaier.ch>
2024-09-30 21:34:05 +0200

    Add admin area

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "Access local instance on http://localhost:5000"
-docker run --rm -t -i -v ./instance:/vds/instance -v ./config.json:/vds/config.json:ro -p 5000:5000 penglmaier/vds-sternwarte
+docker run --rm -t -i \
+	-v ./instance:/vds/instance \
+	-v ./config.json:/vds/config.json:ro \
+	-p 5000:5000 \
+	-e TRUSTED_PROXY="127.0.0.1" \
+	-e TRUSTED_PROXY_HEADERS="forwarded" \
+	penglmaier/vds-sternwarte
---
+++ webapp/admin/utils.py
@@ -0,0 +1,82 @@
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import BaseView, expose, AdminIndexView
+from flask_login import current_user
+
+def names(groups):
+    if groups:
+        str = groups[0].name
+        for g in groups[1:]:
+            str += f", {g.name}"
+        return str
+    else:
+        return "None"
+
+
+class GenericModelView(ModelView):
+    page_size = 50  # the number of entries to display on the list view
+    can_create = True
+    can_edit = True
+    can_delete = True
+    can_view_details = True
+    column_hide_backrefs = False
+    can_export = True
+
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.has_role("admin")
+
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        return redirect(url_for('home', next=request.url))
+
+class UserModelView(GenericModelView):
+    column_list = ('name', 'surname', 'firstname', 'email', 'vds_number', 'groups')
+    column_exclude_list = ['password', ]
+    form_excluded_columns = ['password', ]
+    # column_descriptions = dict(
+    #     name='Username',
+    # )
+    column_labels = dict(
+        name='Username',
+        surname='Surname',
+        firstname="First name",
+        email="Email",
+        vds_number="VdS Number",
+        groups="Groups",
+    )
+    column_formatters = dict(
+        groups=(lambda v, c, m, p: names(m.groups)), # format group list
+    )
+
+
+
+class GroupModelView(GenericModelView):
+    column_list = ('name', 'roles')
+    # column_descriptions = dict(
+    #     name='Name of group',
+    # )
+    column_formatters = dict(
+        roles=(lambda v, c, m, p: names(m.roles)), # format role list
+    )
+
+class RoleModelView(GenericModelView):
+    column_list = ('name', 'groups')
+    # column_descriptions = dict(
+    #     name='Name of role',
+    # )
+    column_formatters = dict(
+        groups=(lambda v, c, m, p: names(m.groups)), # format group list
+    )
+
+class MyHomeView(AdminIndexView):
+    @expose('/')
+    def index(self):
+        return self.render('admin/index.html')
+
+def init_admin(app, admin):
+    from webapp import db
+    from webapp.models import User, Group, Role
+
+    admin.init_app(app, index_view=MyHomeView())
+    admin.add_view(UserModelView(User, db.session))
+    admin.add_view(GroupModelView(Group, db.session))
+    admin.add_view(RoleModelView(Role, db.session))
