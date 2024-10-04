8a12512c7a21e2e29e2a436a0e9784b7fd0857cc
Peter Englmaier <peter@englmaier.ch>
2024-10-04 21:23:23 +0200

    Refactor

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -4,10 +4,10 @@ from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from sqlalchemy.exc import OperationalError
 
 from .config import Config
 from .database import db
+from webapp.setup_users import setup_users
 from webapp.admin.utils import init_admin
 
 
@@ -35,6 +35,11 @@ def create_app(config_class=Config):
     mail.init_app(app)
     init_admin(app, admin) # setup admin views
 
+    @app.before_request
+    def set_global_variables():
+        from .global_vars import set_global_vars
+        set_global_vars()
+
     #from webapp.admin.routes import admin
     from webapp.users.routes import users
     from webapp.posts.routes import posts
@@ -45,63 +50,6 @@ def create_app(config_class=Config):
     app.register_blueprint(main)
     app.register_blueprint(errors)
 
-
-    # setup admin user, if name and password are set
-    # always reset password to configured password
-    pwd_complex = len(config_class.ADMIN_USER) > 3 \
-        and len(config_class.ADMIN_USER) > 3 \
-        and sum(c.isupper() for c in config_class.ADMIN_PASSWORD) > 0 \
-        and sum(c.islower() for c in config_class.ADMIN_PASSWORD) > 0 \
-        and sum(c.isdigit() for c in config_class.ADMIN_PASSWORD) > 0
-
-    try:
-        if pwd_complex:
-            from webapp.models import User, Group, Role
-            ctx=app.app_context()
-            ctx.push()
-            admin_user=User.query.filter_by(name=config_class.ADMIN_USER).first()
-            hashed_password = bcrypt.generate_password_hash(config_class.ADMIN_PASSWORD).decode('utf-8')
-            if admin_user:
-                admin_user.password = hashed_password
-                admin_user.email = config_class.ADMIN_EMAIL
-                db.session.commit()
-            else:
-                print(f"INFO: Create new admin user {config_class.ADMIN_USER}")
-                admin_user = User(name=config_class.ADMIN_USER, email=config_class.ADMIN_EMAIL, password=hashed_password)
-                db.session.add(admin_user)
-                db.session.commit()
-
-            # add admin group, if not present, and assign to admin user
-            admin_group=Group.query.filter_by(name="admin").first()
-            if not admin_group:
-                admin_group = Group(name="admin")
-                admin_user.groups.append(admin_group)
-                db.session.add(admin_group)
-                db.session.add(admin_user)
-                db.session.commit()
-            elif admin_group not in admin_user.groups:
-                admin_user.groups.append(admin_group)
-                db.session.add(admin_user)
-                db.session.commit()
-
-            # add admin role, if not present, and assign to admin group
-            admin_role=Role.query.filter_by(name="admin").first()
-            if not admin_role:
-                admin_role = Role(name="admin")
-                admin_group.roles.append(admin_role)
-                db.session.add(admin_role)
-                db.session.add(admin_group)
-                db.session.commit()
-            elif admin_role not in admin_group.roles:
-                admin_group.roles.append(admin_role)
-                db.session.add(admin_group)
-                db.session.commit()
-
-            db.session.commit()
-            ctx.pop()
-        else:
-            print("WARNING: password is not complex enough, not setting up admin user")
-    except OperationalError:
-        print("WARNING: Database not initialized")
+    setup_users(app, bcrypt)
 
     return app
