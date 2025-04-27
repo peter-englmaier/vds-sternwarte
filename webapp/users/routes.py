ac6cddea502498edd7890fb484963b6193713e99
Peter Englmaier <peter.englmaier@gmail.com>
2025-04-27 15:36:52 +0200

    Fix registration error

--- webapp/users/routes.py
+++ webapp/users/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
-from webapp import bcrypt
+from webapp import bcrypt, db
 from webapp.model.db import User, Post
 from webapp.users import users
 from webapp.users.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
