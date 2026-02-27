2baa39280c5945d4e7b62e2ed5ed81032b3793d7
Peter Englmaier <peter.englmaier@gmail.com>
2026-02-27 15:57:55 +0100

    Initialize required users, groups and roles at each start

--- webapp/users/setup_users.py
+++ webapp/users/setup_users.py
@@ -1,23 +1,54 @@
 from flask import current_app
 from sqlalchemy.exc import OperationalError
 from webapp import db, bcrypt
+from webapp.model.db import User, Group, Role
+from webapp.orders.constants import *
 
+"""
+Setup_users: Create a minimal set of users, groups, and roles, if database is empty.
+             Create admin user, or overwrite admin password, if it already exists.
+             Make sure admin user is in admin_group. This ensures, that application stays
+             functional, even if users/groups are dropped by accident.
+             
+             Note: additional one-time setup is done with the init-db.py script.
+"""
 def setup_users():
-    # setup admin user, if name and password are set
-    # always reset password to configured password
     app=current_app
-    pwd_complex = len(app.config['ADMIN_USER']) > 3 \
-                  and len(app.config['ADMIN_PASSWORD']) > 8 \
-                  and sum(c.isupper() for c in app.config['ADMIN_PASSWORD']) > 0 \
-                  and sum(c.islower() for c in app.config['ADMIN_PASSWORD']) > 0 \
-                  and sum(c.isdigit() for c in app.config['ADMIN_PASSWORD']) > 0
-
     try:
-        if pwd_complex:
-            from webapp.model.db import User, Group, Role
+        with app.app_context():
+            # create all roles and groups needed
+            for choice in USER_ROLE_CHOICES:
+                role_name = choice[0]
+                role = Role.query.filter_by(name=role_name).first()
+                if not role:
+                    role = Role(name=role_name)
+                    db.session.add(role)
+                    db.session.commit()
 
-            with app.app_context():
-                admin_user = User.query.filter_by(name=app.config['ADMIN_USER']).first()
+                # groups are named after roles (e.g.: "admin" role and "admin_group" group)
+                group_name = role_name + "_group"
+                group = Group.query.filter_by(name=group_name).first()
+                if not group:
+                    group = Group(name=group_name)
+                    db.session.add(group)
+                    db.session.commit()
+
+                # add role to group
+                if role not in group.roles:
+                    group.roles.append(role)
+                    db.session.add(group)
+                    db.session.commit()
+
+            # setup admin user, if name and password are set
+            # always reset password to configured password; password must be complex enough
+            pwd_complex = len(app.config['ADMIN_USER']) > 3 \
+                          and len(app.config['ADMIN_PASSWORD']) > 8 \
+                          and sum(c.isupper() for c in app.config['ADMIN_PASSWORD']) > 0 \
+                          and sum(c.islower() for c in app.config['ADMIN_PASSWORD']) > 0 \
+                          and sum(c.isdigit() for c in app.config['ADMIN_PASSWORD']) > 0
+
+            admin_user = User.query.filter_by(name=app.config['ADMIN_USER']).first()
+            if pwd_complex:
                 hashed_password = bcrypt.generate_password_hash(app.config['ADMIN_PASSWORD']).decode('utf-8')
                 if admin_user:
                     admin_user.password = hashed_password
@@ -28,34 +59,20 @@ def setup_users():
                     admin_user = User(name=app.config['ADMIN_USER'], email=app.config['ADMIN_EMAIL'], password=hashed_password)
                     db.session.add(admin_user)
                     db.session.commit()
+            else:
+                print("WARNING: password is not complex enough, cannot set up admin user.")
+                print("         Requirements: at least 9 characters, must contain uppercase/lowercase/digit.")
+            if not admin_user:
+                print("WARNING: NO ADMIN USER DEFINED")
 
-                # add admin group, if not present, and assign to admin user
-                admin_group = Group.query.filter_by(name="admin").first()
-                if not admin_group:
-                    admin_group = Group(name="admin")
+
+            # assign admin_group to admin user
+            if admin_user:
+                admin_group = Group.query.filter_by(name=USER_ROLE_ADMIN + "_group").first()
+                if admin_group not in admin_user.groups:
                     admin_user.groups.append(admin_group)
-                    db.session.add(admin_group)
                     db.session.add(admin_user)
                     db.session.commit()
-                elif admin_group not in admin_user.groups:
-                    admin_user.groups.append(admin_group)
-                    db.session.add(admin_user)
-                    db.session.commit()
 
-                # add admin role, if not present
-                admin_role = Role.query.filter_by(name="admin").first()
-                if not admin_role:
-                    admin_role = Role(name="admin")
-                    db.session.add(admin_role)
-                    db.session.commit()
-
-                # assiggn admin role to admin group if not already assigned
-                if admin_role not in admin_group.roles:
-                    admin_group.roles.append(admin_role)
-                    db.session.add(admin_group)
-                    db.session.commit()
-
-        else:
-            print("WARNING: password is not complex enough, not setting up admin user")
     except OperationalError:
         print("WARNING: Database not initialized")
