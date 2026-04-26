60a239c166a28ddac9aaad6493640072f2e31813
Peter Englmaier <peter.englmaier@gmail.com>
2026-04-26 22:43:45 +0200

    Make proxy configuration generic behind a single proxy

--- prod.py
+++ prod.py
@@ -14,8 +14,8 @@ serve(TransLogger(app, setup_console_handler=(os.getenv('SETUP_CONSOLE_HANDLER',
     listen=os.getenv('LISTEN', '0.0.0.0:5000'),
     url_scheme=os.getenv('URL_SCHEMA', 'https'),
     threads=int(os.getenv('THREADS','4')),
-    trusted_proxy_count=int(os.getenv('TRUSTED_PROXY_COUNT', '1')),
-    trusted_proxy=os.getenv('TRUSTED_PROXY', '172.22.0.6'),
+    trusted_proxy_count=int(os.getenv('TRUSTED_PROXY_COUNT', '1')), # trust only one proxy, set to 0 when not using proxy
+    trusted_proxy=os.getenv('TRUSTED_PROXY', '*'), # trust all proxies, if IP of proxy can change
     trusted_proxy_headers=os.getenv('TRUSTED_PROXY_HEADERS', 'x-forwarded-for x-forwarded-host x-forwarded-proto x-forwarded-port'),
     log_untrusted_proxy_headers=(os.getenv('LOG_UNTRUSTED_PROXY_HEADERS', 'true').lower() in ('true', '1', 't'))
     )
