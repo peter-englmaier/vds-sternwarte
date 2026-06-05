63fa24c3cf038ad67d253bb582921eee65b42bf8
Peter Englmaier <peter.englmaier@gmail.com>
2026-06-05 22:59:41 +0200

    Fix Approver list

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,41 +1,28 @@
 #!/bin/sh
 echo "Running docker-startup.sh"
 export FLASK_APP=prod.py
-# Make sure, parameters are set either true or false
-[ -z "$RUNFLASK" ] && RUNFLASK=true
-[ -z "$RUNCELERY" ] && RUNCELERY=true
-[ -z "$RUNBEAT" ] && RUNBEAT=true
 
-if [  "$RUNFLASK" != "true" -a "$RUNFLASK" != "false" ]; then
-  echo "WRONG PARAMETER RUNFLASK=$RUNFLASK"
-  exit 1
-fi
-
-if [  "$RUNCELERY" != "true" -a "$RUNCELERY" != "false" ]; then
-  echo "WRONG PARAMETER RUNCELERY=$RUNCELERY"
-  exit 1
-fi
-
-
-if $RUNCELERY; then
-  echo "Run celery worker"
-  if $RUNFLASK; then #run in background
+if [ "$RUNCELERY" == "true" ]; then
+  if [ "$RUNFLASK" == "true" ]; then
+    echo "Run celery worker in background"
     celery -A make_celery worker -l info &
   else
+    echo "Run celery worker"
     exec celery -A make_celery worker -l info
   fi
 fi
 
-if $RUNBEAT; then
-  echo "Run celery beat"
-  if $RUNFLASK; then #run in background
+if [ "$RUNBEAT" == "true" ]; then
+  if [ "$RUNFLASK" == "true" ]; then
+    echo "Run celery beat in background"
     celery -A make_celery beat -l info -s instance/celerybeat-schedule  &
   else
+    echo "Run celery beat"
     exec celery -A make_celery beat -l info -s instance/celerybeat-schedule
   fi
 fi
 
-if $RUNFLASK; then
+if [ "$RUNFLASK" == "true" ]; then
   echo "Upgrade database if needed"
   flask --app app.py db upgrade
 
