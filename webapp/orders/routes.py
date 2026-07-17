8a70d926eceea44fa6a1ca6e1fbb14475d83b531
ybastian <bastianschwarzenbach@web.de>
2026-07-18 00:09:38 +0200

    Anpassung Teil 2

--- webapp/orders/routes.py
+++ webapp/orders/routes.py
@@ -118,8 +118,6 @@ def show_orders():
         pwuser = User.query.get(order.request_poweruser_id)
         if pwuser:
             order.poweruser_name = pwuser.display_name()
-        else:
-            order.status_label = ORDER_STATUS_LABELS.get(order.status, "??")
     return render_template("orders.html", title="Teleskopzeit Beantragung", orders=user_orders)
 
 
