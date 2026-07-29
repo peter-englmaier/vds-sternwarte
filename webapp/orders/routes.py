fa516b4217cc1da16ef4b528acfbb20233d15997
Jost Jahn <amrum-software@jostjahn.de>
2026-07-29 23:15:41 +0200

    Restrict observation request overview access

--- webapp/orders/routes.py
+++ webapp/orders/routes.py
@@ -925,6 +925,7 @@ def show_calendar():
 
 @orders.route("/obs_requests")
 @login_required
+@role_required("approver")
 def obs_request_complete():
     orders = ObservationRequest.query.order_by(ObservationRequest.id.desc()).all()
 
